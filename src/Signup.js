ff4c0eee3893ddaa569e5bc44226617c4e48aee6
mzwang25 <mzwang25@gmail.com>
2021-05-15 14:02:18 -0700

    redirect to local or serval

--- src/Signup.js
+++ src/Signup.js
@@ -1,10 +1,18 @@
 import React, {Component } from "react";
 
 const my_client_id = "025272b187b744a88a3514d168b0e71d";
-const redirect_uri = "http://localhost:3000/signup";
+const local_redirect_uri = "http://localhost:3000/signup";
+const server_redirect_uri = "https://getyourmusicbuzz.web.app/signup";
 
 function link() {
   var scopes = 'user-read-private user-read-email streaming user-library-read user-library-modify user-read-playback-state user-modify-playback-state'
+
+  var redirect_uri = "";
+  if(window.hostname = "localhost") {
+    redirect_uri = local_redirect_uri;
+  } else {
+    redirect_uri = server_redirect_uri;
+  }
   return (
     "https://accounts.spotify.com/authorize" +
     "?response_type=token" +
